deefbf450d435912f8b5250fbb320b07a64d113b
chengpanwang <chengpan168@126.com>
2021-01-19 09:57:35 +0800

    Initial Commit

---
+++ redis-pro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  redis-pro
+//
+//  Created by chengpanwang on 2021/1/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ redis-pro/redis_proApp.swift
@@ -0,0 +1,17 @@
+//
+//  redis_proApp.swift
+//  redis-pro
+//
+//  Created by chengpanwang on 2021/1/19.
+//
+
+import SwiftUI
+
+@main
+struct redis_proApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

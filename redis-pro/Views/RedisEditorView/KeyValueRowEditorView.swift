4e5080502070a824d908ae9b3ed395a8d9e3f885
chengpanwang <chengpan168@126.com>
2021-04-09 17:44:35 +0800

    keys

--- redis-pro/Views/RedisEditorView/KeyValueRowEditorView.swift
+++ redis-pro/Views/RedisEditorView/KeyValueRowEditorView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct KeyValueRowEditorView: View {
+    @State var text:String = ""
+    @State var hashMap:[String: String] = ["testesttesttesttesttesttesttesttesttesttesttestt":"234243242343", "test1":"2342"]
+    
     var body: some View {
-        HStack {
-            Text("key")
-            Text("value")
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(alignment: .center , spacing: 4) {
+                IconButton(icon: "plus", name: "Add", action: onDeleteAction)
+                IconButton(icon: "trash", name: "Delete", action: onDeleteAction)
+                Spacer()
+            }
+            .padding(EdgeInsets(top: 6, leading: 6, bottom: 6, trailing: 6))
+            
+            List() {
+//                TextField("test", text: $text).environment(\.isEnabled, true)
+//                    .textFieldStyle(PlainTextFieldStyle())
+                LazyVGrid(columns: Array(repeating: GridItem(), count: 2), alignment: .leading, spacing: 20) {
+                ForEach(hashMap.sorted(by: >), id:\.key) { key, value in
+//                    Text(key)
+//                        .multilineTextAlignment(.leading)
+                    TextField("Key", text: $text)
+                    Text(value)
+                        .multilineTextAlignment(.leading)
+                }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .padding(.all, 0)
+//                    .frame(minWidth: 100, maxWidth: .infinity, minHeight: 100, maxHeight: .infinity, alignment: .topLeading)
+
+            
         }
+    }
+    
+    func onDeleteAction() -> Void {
+        print("hash field delete action...")
     }
 }
 
